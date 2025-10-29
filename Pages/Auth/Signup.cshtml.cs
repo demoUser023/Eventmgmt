0f21eb8edf02d0aa066c528a7f43ecec59a01f82
aashish <aashishmyself636@gmail.com>
2025-10-29 15:29:11 +0545

    static changes

---
+++ Pages/Auth/Signup.cshtml.cs
@@ -0,0 +1,19 @@
+using System.Dynamic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EventManagement.Pages.Auth;
+
+public class SignModel : PageModel
+{
+    
+    public void OnGet()
+    {
+
+
+    }
+    
+    public void OnPost()
+    {
+        
+    }
+}
